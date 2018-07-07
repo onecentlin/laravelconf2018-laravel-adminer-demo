fbc3730329e6e2b61975288fdc146b10c49c22ff
Winnie Lin <onecentlin@gmail.com>
2018-07-08 00:11:59 +0800

    Authentication table migration
    $ php artisan migration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // MySQL 5.7.6 以下需設定以解決 utf8mb4 編碼問題
+        Schema::defaultStringLength(191);
     }
 
     /**
